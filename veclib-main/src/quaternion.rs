50473d090754d730883a8bf4d1bb19c6fc0877db
Jedjoud10 <34755598+jedjoud10@users.noreply.github.com>
2021-08-27 19:25:49 -0400

    Identity quaternion

--- veclib-main/src/quaternion.rs
+++ veclib-main/src/quaternion.rs
@@ -1,6 +1,12 @@
-use crate::Vector4;
+use crate::{Vector4, types::DefaultStates};
 
 // A quaternion that represents a rotation
 pub struct Quaternion<T> {
     data: Vector4<T>,
+}
+
+// Da code
+impl<T> Quaternion<T> where T: DefaultStates + Clone + Copy + Sized {
+    // Constants
+    pub const IDENTITY: Quaternion<T> = Quaternion { data: Vector4::<T>::W };
 }
